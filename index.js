0e43f4c6672e9e547f15a6bb936902ec196d2162
Fabian A Ortiz Chavez <alonsofabianortiz@icloud.com>
2025-08-28 00:43:32 -0600

    palidrome

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(str) {
+let left = 0;
+let right = str.length - 1;
+
+while (left < right) {
+  if (str[left] !== str[right]) {
+    return false;
+  }
+  left++;
+  right--;
 }
+return true;
+}
+
+module.exports = isPalindrome;
+
+  // Write your algorithm here
+//Splitting the string into an array of characters, Reversing the array, Joining it back into a string, Then comparing it to the original string.
 
 /* 
   Add your pseudocode here
 */
 
+
+
 /*
   Add written explanation of your solution here
-*/
+*///Splitting the string into an array of characters then reversing the array, joining it back into a string then comparing it to the original string.
 
 // You can run `node index.js` to view these console logs
-if (require.main === module) {
+
   // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
-}
-
-module.exports = isPalindrome;
